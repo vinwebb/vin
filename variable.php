b25a242a665b4df72820b2d6562bb6f74cc80b95
vinwebb <vintechwebb01@gmail.com>
2023-01-06 11:58:26 +0100

    First Commit

---
+++ variable.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP | Variables </title>
+</head>
+<body>
+    <!-- Unlike variable, the value assigned to a constant can't be changed or redefined . -->
+
+
+    <?php
+
+        $name = "Onyejekwe Vincent Somkele";
+        $age = 30;
+        $height = 5.7;
+
+        echo "My name is {$name}, i am {$age} years old and i am {$height}fit in height.";
+
+    ?>
+</body>
+</html>
