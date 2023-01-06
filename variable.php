c2511b9ec90b3045abc29aa2eed573ed3192decb
vinwebb <vintechwebb01@gmail.com>
2023-01-06 12:27:56 +0100

    added features

--- variable.php
+++ variable.php
@@ -7,17 +7,22 @@
     <title>PHP | Variables </title>
 </head>
 <body>
-    <!-- Unlike variable, the value assigned to a constant can't be changed or redefined . -->
+    <!-- Unlike constants, the value assigned to a variable can change or be redefined . -->
+    <!-- Variables are denoted using a dollar sign "$" -->
+    <!-- Variables are case sensitive -->
+    <!-- you can't start a variable with number or special characters "$2name" -->
+    <!-- Instead a variable can contain numbers and other special characters in them. example "$project1", "$male_names" etc -->
+
 
 
     <?php
 
         $name = "Onyejekwe Vincent Somkele";
-        $age = 30;
+        $age = 25;
         $height = 5.7;
 
-        echo "My name is {$name}, i am {$age} years old and i am {$height}fit in height.";
-
+        echo "My name is {$name}, i am {$age} years old and i am {$height}fit in height.". "</br>";
+        echo "{$name}";
     ?>
 </body>
 </html>
